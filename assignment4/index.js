55e254e5e758caedc61dc7f478eece62b3686612
Shivhare-eng <79094774+Shivhare-eng@users.noreply.github.com>
2024-04-30 16:17:59 +0530

    Delete assignment4 directory

--- assignment4/index.js
+++
@@ -1,7 +0,0 @@
-console.log("// Creation of Object in js ::module.exports=Product;");
-
-const product1=require('./calc');
-
-const p2=new product1((10,40),(40,10),(10,40),(40,10));
-
-console.log(p2.display());
