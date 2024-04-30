d6c3ede6c47e2aebe6563de8bf8616594ea236af
Shivhare-eng <79094774+Shivhare-eng@users.noreply.github.com>
2024-04-30 16:16:01 +0530

    Delete assignment1 directory

--- assignment1/server.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-const port = 3000;
-http.createServer((req,res)=>{
-    res.write("<h1>Good morning</h1>");
-    res.end();
-}).listen(port,()=>{
-    console.log(`Server is running at htttp://localhost:${port}`);
-})
